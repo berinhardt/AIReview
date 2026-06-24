c96f96d9bc7fd13ec880340dd9d0bb0fb16f9918
Pablo Amorin <darta@web2mil.com.uy>
2026-06-23 22:04:11 -0300

    Interactions API

--- models/Google.js
+++ models/Google.js
@@ -1,52 +1,64 @@
 import { GoogleGenAI, ThinkingLevel } from "@google/genai";
-async function GeminiLLMRequest(model, SystemPrompt, Input) {
+async function GeminiLLMRequest(model, SystemPrompt, Input, previousInteractionId = null) {
    if (model.context && SystemPrompt.length + Input.length >= model.context * 4) {
       throw new Error("Input too large");
    }
    const APIKEY = process.env.GEMINI_API_KEY;
    if (!APIKEY) throw new Error("NO API KEY FOUND!");
    const client = new GoogleGenAI({ apiKey: APIKEY });
-   const response = await client.models.generateContent({
+   const interaction = await client.interactions.create({
       model: model.name,
-      config: {
-         systemInstruction: SystemPrompt,
+      system_instruction: SystemPrompt,
+      input: Input,
+
+      ...(previousInteractionId && {previousInteractionId}),
+
+      generation_config: {
          temperature: 0.1,
-         thinkingConfig: {
-            thinkingLevel: ThinkingLevel.HIGH
-         }
+         thinking_level:ThinkingLevel.HGIH
       },
-      contents: Input,
    });
 
    const result = {
-      text: response.text,
-      cost: calculateCosts(model, response.usageMetadata)
+      id: interaction.id,
+      text: interaction.output_text,
+      cost: calculateCosts(model, interaction.usage)
    };
 
    return result;
 }
 function calculateCosts(model, usage) {
-   const { promptTokenCount = 0, candidatesTokenCount = 0, thoughtsTokenCount = 0 } = usage || {};
+   const { 
+      total_input_tokens, totalInputTokens,
+      total_output_tokens, totalOutputTokens,
+      total_thought_tokens, totalThoughtTokens 
+   } = usage || {};
+   
+   const promptTokens = totalInputTokens ?? total_input_tokens ?? 0;
+   const outputTokens = totalOutputTokens ?? total_output_tokens ?? 0;
+   const thoughtTokens = totalThoughtTokens ?? total_thought_tokens ?? 0;
+
    const { input = 0, output = 0 } = model;
-   const cost = (promptTokenCount * input + (candidatesTokenCount + thoughtsTokenCount) * output) / 1e6;
+   
+   const cost = (promptTokens * input + (outputTokens + thoughtTokens) * output) / 1e6;
    return Number(cost.toFixed(6));
 }
 
-export async function Gemini31FlashLite(SystemPrompt, Input) {
+export async function Gemini31FlashLite(SystemPrompt, Input, id = null) {
    return GeminiLLMRequest({
       name: "gemini-3.1-flash-lite",
       context: 1048576,
       input: 0.25,
       output: 1.5
-   }, SystemPrompt, Input);
+   }, SystemPrompt, Input, id);
 }
 
-export async function Gemini31Pro(SystemPrompt, Input) {
+export async function Gemini31Pro(SystemPrompt, Input, id = null) {
    return GeminiLLMRequest({
       name: "gemini-3.1-pro-preview",
       context: 1048576,
       input: 2,
       output: 12,
-   }, SystemPrompt, Input);
+   }, SystemPrompt, Input, id);
 }
 
